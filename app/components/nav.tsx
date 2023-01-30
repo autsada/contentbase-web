f555af3acf8ae1354b19e50ad9676aec9860f2f3
Autsada T <autsada@gmail.com>
2023-01-30 12:23:48 +0700

    update nav bar styles

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,30 +1,37 @@
 import { Link, useLocation } from "@remix-run/react"
+import { MdPerson } from "react-icons/md"
+import { IoSearchOutline } from "react-icons/io5"
 import type { UserRecord } from "firebase-admin/auth"
 
 export function Nav({ user }: { user: UserRecord | null }) {
   const { pathname } = useLocation()
 
   return (
-    <div className="w-screen h-[70px] px-3 flex items-center border-b border-borderExtraLightGray shadow-neutral-300">
-      <div className="h-full flex items-center justify-center">
-        <div className="w-[60px] h-[60px] rounded-full overflow-hidden">
-          <Link to="/">
-            <img
-              src="/logo.png"
-              alt="CTB"
-              className="w-full h-full object-cover"
-            />
-          </Link>
-        </div>
+    <div className="w-full h-[70px] px-4 flex items-center justify-between gap-x-5 border-b border-borderExtraLightGray shadow-neutral-300">
+      <div className="w-[70px] h-full flex items-center justify-start">
+        <Link to="/">
+          <img
+            src="/logo.png"
+            alt="CTB"
+            className="w-[55px] h-[55px] rounded-full object-fit"
+          />
+        </Link>
       </div>
-      <div className="flex-grow mx-3">
+
+      <div className="relative h-[50px] flex-grow flex items-center justify-between rounded-full overflow-hidden">
         {!pathname.startsWith("/auth") && (
-          <div className="h-[45px] flex items-center justify-center bg-neutral-50 rounded-full">
-            Search
-          </div>
+          <>
+            <input
+              type="text"
+              className="block h-full w-full pl-30 outline-none focus:outline-none"
+            />
+
+            <IoSearchOutline size={25} className="absolute right-0" />
+          </>
         )}
       </div>
-      <div className="h-full flex items-center justify-center">
+
+      <div className="w-[60px] h-full flex items-center justify-end">
         {pathname.startsWith("/auth") ? (
           <Link to="auth" replace={true} className="px-4">
             &#10005;
@@ -38,14 +45,11 @@ export function Nav({ user }: { user: UserRecord | null }) {
                 </button>
               </Link>
             ) : (
-              <form method="post" action="/logout">
-                <button
-                  type="submit"
-                  className="btn-orange text-sm rounded-3xl w-max h-8 px-4"
-                >
-                  Logout
-                </button>
-              </form>
+              <div className="w-[40px] h-[40px] p-4 bg-neutral-100 rounded-full flex items-center justify-center overflow-hidden">
+                <Link to="/profile">
+                  <MdPerson size={30} color="#3f3f46" />
+                </Link>
+              </div>
             )}
           </>
         )}
