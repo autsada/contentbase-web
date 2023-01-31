9ec748f4a4ecdff48dfd762bc337dacd0f9c7790
Autsada T <autsada@gmail.com>
2023-01-31 19:05:57 +0700

    add create profile page

---
+++ app/routes/profile/create.tsx
@@ -0,0 +1,42 @@
+export default function CreateProfile() {
+  return (
+    <div className="page p-4 text-start">
+      <form className="px-5">
+        <h6 className="text-center mb-4">Create Profile</h6>
+
+        <fieldset className="mb-6 border border-borderDarkGray pl-4 rounded-md bg-white">
+          <legend className="text-textExtraLight px-1">
+            Handle
+            <abbr title="This field is mandatory" aria-label="required">
+              *
+            </abbr>
+          </legend>
+          <label htmlFor="handle">
+            <input
+              type="text"
+              name="handle"
+              placeholder="What you would like to be called?"
+              className="block w-full h-10 font-semibold text-blueDark text-lg outline-none placeholder:font-light placeholder:text-blue-400 placeholder:text-sm"
+            />
+          </label>
+        </fieldset>
+
+        <fieldset className="mb-6 border border-borderDarkGray pl-4 rounded-md bg-white">
+          <legend className="text-textExtraLight px-1">Profile Image</legend>
+          <label htmlFor="handle">
+            <input
+              type="text"
+              name="handle"
+              placeholder="What you would like to be called?"
+              className="block w-full h-10 font-semibold text-blueDark text-lg outline-none placeholder:font-light placeholder:text-blue-400 placeholder:text-sm"
+            />
+          </label>
+        </fieldset>
+
+        <button type="submit" className="btn-dark w-40 rounded-full">
+          Create Profile
+        </button>
+      </form>
+    </div>
+  )
+}
--- app/routes/profile/index.tsx
+++ app/routes/profile/index.tsx
@@ -6,15 +6,52 @@ export default function Profiles() {
   const { account, hasProfile } = useAccountContext()
 
   return (
-    <div className="page py-5">
-      <h6>Profiles</h6>
+    <div className="page text-start">
+      <div className="w-full p-4 border-b-[1px] border-borderLightGray">
+        <h6 className="text-base">Your Profiles</h6>
+        <div className="w-full py-2 px-4">
+          {!hasProfile && (
+            <>
+              <p className="font-light text-textLight">
+                You don't have any profile yet.
+              </p>
+            </>
+          )}
+        </div>
+        <div className="mt-4 mb-2">
+          <Link to="create">
+            <button className="btn-dark w-40 rounded-full">
+              Create Profile
+            </button>
+          </Link>
+        </div>
+      </div>
 
       {account && typeof hasProfile === "boolean" && !hasProfile && (
         <BackdropWithInfo>
-          <h6 className="text-center text-base">Create your first profile</h6>
-          <div className="mt-6">
-            <Link to="/">
-              <button>Maybe Later</button>
+          <div className="px-2">
+            <p className="mb-2 text-blueBase">
+              You will need a profile to upload, share, like, and comment on
+              ContentBase.
+            </p>
+            <h6 className="text-base">
+              Would you like to create your first profile now?
+            </h6>
+          </div>
+
+          <div className="my-6">
+            <Link to="create">
+              <button className="btn-dark w-36 rounded-full">
+                Yes, please
+              </button>
+            </Link>
+          </div>
+
+          <div className="mb-2">
+            <Link to="/" className="cursor-pointer">
+              <h6 className="text-orange-400 text-center text-base">
+                Maybe Later
+              </h6>
             </Link>
           </div>
         </BackdropWithInfo>
